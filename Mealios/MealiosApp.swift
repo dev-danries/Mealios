4cd8e8f5135bf4eaafc4d26e0d4634779f5826ab
Dan Ries <daniel.n.ries@gmail.com>
2023-07-28 18:19:20 -0400

    Initial Commit

---
+++ Mealios/MealiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealiosApp.swift
+//  Mealios
+//
+//  Created by Daniel Ries on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MealiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
